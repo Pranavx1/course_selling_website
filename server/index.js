a2645b107f4ff49dbc1048f395cb530e5256746a
Pranavx1 <129819783+Pranavx1@users.noreply.github.com>
2025-03-29 02:42:31 +0530

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "https://frontend-final-5863.onrender.com" }));
 app.use(express.json());
 
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
-const url = process.env.mongoose_url || "mongodb+srv://pranav136:Pranavgarg136@cluster0.j2jt8ke.mongodb.net/"
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+const url =
+  process.env.mongoose_url ||
+  "mongodb+srv://pranav136:Pranavgarg136@cluster0.j2jt8ke.mongodb.net/";
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "courses",
+});
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
